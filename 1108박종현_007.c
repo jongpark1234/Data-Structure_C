39b67c79069d76df436a8062948290cc1ef112b9
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-14 11:00:02 +0900

    Create 1108박종현_007.c

---
+++ 1108박종현_007.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+void input(int a[], int cnt)
+{
+	for (int i = 0; i < cnt; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+}
+
+void output(int a[], int cnt)
+{
+	int i;
+
+	for (int i = 0; i < cnt; i++)
+	{
+		printf("%d ", a[i]);
+	}
+}
+
+void swap(int* x, int* y)
+{
+	int t = *x;
+	*x = *y;
+	*y = t;
+}
+
+void selectionSort(int a[], int cnt) 
+{
+	for (int i = 0; i < cnt - 1; i++)
+	{
+		for (int j = i + 1; j < cnt; j++)
+		{
+			if (a[i] > a[j])
+				swap(&a[i], &a[j]);
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	int a[100];
+
+	scanf("%d", &n);
+	input(a, n);
+	selectionSort(a, n);
+	output(a, n);
+	return 0;
+}
