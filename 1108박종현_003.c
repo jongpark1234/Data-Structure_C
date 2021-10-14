bf2d9112647945d133858e540a163c9ff1cd2ae0
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-14 10:58:09 +0900

    Create 1108박종현_003.c

---
+++ 1108박종현_003.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	static int i = 7, * p, ** pp;
+
+	p = &i;
+	pp = &p;
+
+	printf("p\'s Address = %p\n", p);
+	printf("pp\'s Address = %p\n", pp);
+
+	(*p)++;
+
+	printf("i = %d\t", i);
+	printf("*p = %d\t", *p);
+	printf("**pp = %d\n", **pp);
+
+	(**pp)++;
+	printf("i = %d\t", i);
+	printf("*p = %d\t", *p);
+	printf("**pp = %d\n", **pp);
+	
+	return 0;
+}
